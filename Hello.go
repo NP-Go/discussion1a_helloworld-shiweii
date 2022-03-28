adcf5dba4efc20f7dc58fb9c98ab95fb54d86530
Ou Shiwei <shiwei.ou@protonmail.com>
2022-03-28 11:15:14 +0800

    Added Message

--- Hello.go
+++ Hello.go
@@ -1,8 +1,13 @@
 package main
 
+import (
+	"fmt"
+)
+
 func HelloWorld() string {
 	//Insert your code here
-
+	messageHelloWorld := "Hello World from Go !!"
+	fmt.Println(messageHelloWorld)
 	//Do not remove this line here
 	return messageHelloWorld
 }
